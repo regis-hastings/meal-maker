d8c91c76e9fe9dd54db79d0f577c6c020eff9d42
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-04 10:21:43 -0700

    first commit

---
+++ meal-maker.js
@@ -0,0 +1,28 @@
+const menu = {
+  _courses = {
+    _appetizers: [],
+    _mains: [],
+    _desserts: [],
+    get appetizers() {
+      
+    },
+    set appetizers() {
+      
+    },
+    get mains() {
+      
+    },
+    set mains() {
+      
+    },
+    get desserts() {
+      
+    },
+    set desserts() {
+      
+    }
+  },
+  get courses() {
+    
+  }
+};
